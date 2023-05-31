606f0303c3cb2a052aa59200d792a98bc3b89022
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-05-31 21:56:27 +0530

    JavaScript

---
+++ 2-var_vs_let.js
@@ -0,0 +1,20 @@
+var h = "Harry";
+{
+  var h = "this";
+  console.log(h);
+}
+console.log(h);
+
+let i = "Harry";
+{
+  let i = "this";
+  console.log(i);
+}
+console.log(i);
+
+const j = "Harry";
+{
+  const j = "this";
+  console.log(j);
+}
+console.log(j);
---
+++ 3-Data_types.js
@@ -0,0 +1,12 @@
+
+//nn bb ss u
+
+let a=null;
+let b=345;
+let c=true;
+let d=BigInt("567");
+let e = "Harry";
+let f = Symbol("I am a nice symbol");
+let g
+console.log(a,b,c,d,e,f,g)
+console.log(typeof c)
