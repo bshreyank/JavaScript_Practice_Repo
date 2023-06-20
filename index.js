ae7e0f17b556088d6aaf640d186cc070694c4377
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-06-20 10:45:31 +0530

    JS

--- index.js
+++ index.js
@@ -17,3 +17,30 @@ var z=++x;
 console.log(x);
 console.log(y);
 console.log(z);
+
+
+/////////////////////////////////////////
+
+// toString()
+// join()
+
+var emp_data=["A","B","C"]
+
+console.log(emp_data.toString(),emp_data.join('#'))
+
+
+console.log(emp_data.toString("#", emp_data))
+
+///////////////////////////////////////////////////
+//vaibhav
+ function vaibhav(arr_v){
+  return arr_v;
+}
+
+var arr_v =[1,2,"Shreyank","aaa"];
+vaibhav(arr_v);
+
+
+(function(){return "Srr";
+})
+();
